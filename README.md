a3d355cc42e7ba8ee01bc45ec610e8dc3295661f
shukri1236 <shukri1236>
2024-07-19 19:45:10 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Hospital-s-Lost-and-Found-Application
+**Project name: Hospital's Lost and Found Application**
+
+**Project owner**:  Fatuma Hassan
+
+**Problem Statement:**
+
+Lost items in hospitals disrupt patient care, cause stress, and burden staff. The lack of an efficient recovery process leads to inconvenience and potential safety issues, hindering daily hospital activities.
+
+**Proposed Solution:**
+
+An online application with a user-friendly interface where patients, visitors, and staff can search for lost items. It uses a centralized database to quickly match and locate found items, streamlining the retrieval process.
+
+**Key Features:**
+
+1. Image-based item search using Unsplash API
+
+2. Pagination for browsing results
+
+3. Dynamic content generation
+
+4. "Retrieve Item" functionality
+
+5. Responsive design
+
+6. User-friendly search interface
+
+7. Real-time alerts for retrieval requests
+
+This solution aims to minimize time and effort in recovering lost items, fostering a more efficient and stress-free hospital environment.
+
+**Tools**  
+
+html css js and unplash Api
--- script.js
+++ script.js
@@ -1,1 +1,72 @@
+const accessKey = "LraoWLdcBzxM7d6OAdw9YwN701bjAOACjOnfMG63ZaE";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
 
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12;`
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+    
+
+    if (page === 1) {
+        searchResult.innerHTML = "";
+    }
+    const results = data.results;
+
+
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+
+        const individualItem = document.createElement("div");
+
+        const retriveButton = document.createElement("button");
+        retriveButton.classList.add("retrieve-button");
+        retriveButton.innerHTML = "Retrieve Item";
+
+        retriveButton.addEventListener('click', ()=>{
+            alert("Request has been sent to the administration")
+        })
+
+        individualItem.appendChild(image);
+        individualItem.appendChild(retriveButton);
+        searchResult.appendChild(individualItem);
+    });
+// after fetching the data show more button appears
+    showMoreBtn.style.display ="block";
+}
+
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click", ()=>{
+    page++;
+    searchImages();
+})
+
+
+// OPEN MENU IN SMALL SCREENS
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
+
+const sideMenu = document.getElementById("sideMenu");
+
+menuIcon.onclick = function(){
+    sideMenu.style.right = "0";   
+}
+
+closeIcon.onclick = function(){
+    sideMenu.style.right = "-200px";   
+}
